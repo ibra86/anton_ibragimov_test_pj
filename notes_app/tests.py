c77e5debbd2b3bda65a5f9df3831a2324c01c905
Anton Ibragimov <ibrahimov.ag@gmail.com>
2017-05-16 22:18:37 +0200

    added test for home page

--- notes_app/tests.py
+++ notes_app/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.core.urlresolvers import reverse
+from models import Note
+
 
 # Create your tests here.
+		
+class TestNoteList(TestCase):
+
+	def setUp(self):
+		note1, created = Note.objects.get_or_create(
+			title='lorem ipsum',
+			text='sadfasdfasdfasdf')
+		
+		self.client = Client()
+		
+		self.url = reverse('home')
+	
+	def test_notes_list(self):
+		response = self.client.get(self.url)
+		
+		# checking response status
+		self.assertEqual(response.status_code, 200)
+		
+		# checking content
+		self.assertIn('lorem ipsum', response.content)
+		self.assertIn('sadfasdfasdfasdf', response.content)
+		self.assertIn('published', response.content)
--- notes_app/views.py
+++ notes_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Note
+
 
 # Create your views here.
+class NoteList(ListView):
+	model = Note
+	template_name = 'note_list0.html'
+	context_object_name = 'notes'
