a7397f6050bd804624588a1377494b1eaa500510
Anton Ibragimov <ibrahimov.ag@gmail.com>
2017-05-17 22:57:43 +0200

    notes list home to be deployed

--- mysite_proj/settings.py
+++ mysite_proj/settings.py
@@ -120,6 +120,9 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+PORTAL_URL = 'http://localhost:8000'
 
 
 
@@ -141,3 +144,5 @@ try:
     from .local_settings import *
 except ImportError:
     pass
+
+print DEBUG
--- notes_app/views.py
+++ notes_app/views.py
@@ -6,5 +6,5 @@ from .models import Note
 # Create your views here.
 class NoteList(ListView):
 	model = Note
-	template_name = 'note_list0.html'
+	template_name = 'note_list.html'
 	context_object_name = 'notes'
