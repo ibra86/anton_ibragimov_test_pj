2b20579facd8a17e2fde55b8ba14dec4600ce2d0
Anton Ibragimov <ibrahimov.ag@gmail.com>
2017-05-25 22:35:54 +0200

    small corections for Final version

--- notes_app/static/js/main.js
+++ notes_app/static/js/main.js
@@ -2,7 +2,7 @@ function initNewNote() {
     $("#id-note-form").ajaxForm({
         success: function(data, status, xhr) {
             // $(data).find(".form-saved").html('<div class="alert alert-success" role="alert">New note have been added!</div>')
-            data = data.replace('<p></p>', '<div class="alert alert-success" role="alert">New note have been added!</div>');
+            data = data.replace('<p></p>', '<div class="alert alert-success" role="alert">New note has been added!</div>');
             $("body").html(data);
         },
         error: function(xhr, errmsg, err) {
@@ -12,7 +12,7 @@ function initNewNote() {
 }
 
 function initNewRequestStat() {
-    
+
     var requests_url = (function() {
         var scripts = document.getElementsByTagName("script");
         for (index = 0; index <= scripts.length - 1; ++index) {
