ccf4c52daed9ec3e51319c0be7b82dbd84f37dca
Anton Ibragimov <ibrahimov.ag@gmail.com>
2017-06-15 12:58:39 +0200

    ad-hoc clean up

--- mysite_proj/routing.py
+++
@@ -1,8 +0,0 @@
-from channels.routing import route
-from notes_app.consumers import ws_connect, ws_message, ws_disconnect
-
-channel_routing = [
-    route('websocket.connect', ws_connect),
-    route('websocket.receive', ws_message),
-    route('websocket.disconnect', ws_disconnect),
-]
